ebdf8d13f163ca5666fec0032e9119fd848f9c4b
Neil Scheuermann <neilscheuermann@gmail.com>
2024-02-01 20:33:13 -0700

    test: assert button text, WIP for event hanlders

--- test/sleeping_queens_interface_web/live/home_live_test.ex
+++ test/sleeping_queens_interface_web/live/home_live_test.ex
@@ -9,4 +9,45 @@ defmodule SleepingQueensInterfaceWeb.HomeLiveTest do
     assert view.module == SleepingQueensInterfaceWeb.HomeLive
     assert render(view) =~ "Sleeping Queens"
   end
+
+  test "renders a create game button", %{conn: conn} do
+    {:ok, view, _html} = live(conn, "/")
+
+    assert render(view) =~ "Create Game"
+  end
+
+  test "renders a join game button", %{conn: conn} do
+    {:ok, view, _html} = live(conn, "/")
+
+    assert render(view) =~ "Join Game"
+  end
+
+  # TODO>>>>
+  # test "shows create game modal when button is clicked", %{conn: conn} do
+  #   {:ok, view, _html} = live(conn, "/")
+  #
+  #   # Assert that the create game modal is initially not present
+  #   # refute render(view) =~ "Create New Game"
+  #   # # assert view |> find_modal("create_game_modal") |> is_nil()
+  #
+  #   # Click the "Create Game" button
+  #   # Assert that the create game modal is now present
+  #   assert view
+  #          |> element("button", "Create Game")
+  #          |> render_click() =~ "Create New Game"
+  # end
+
+  # test "shows join game modal when button is clicked", %{conn: conn} do
+  #   {:ok, view, _html} = live(conn, "/")
+  #
+  #   # Assert that the join game modal is initially not present
+  #   assert view |> find_modal("join_game_modal") |> is_nil()
+  #
+  #   # Click the "Join Game" button
+  #   view
+  #   |> click_button("Join Game")
+  #
+  #   # Assert that the join game modal is now present
+  #   assert view |> find_modal("join_game_modal") |> is_not_nil()
+  # end
 end
