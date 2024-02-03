356c06259fa72f2e4ba243509b496873fef47cfc
Neil Scheuermann <neilscheuermann@gmail.com>
2024-02-02 20:59:50 -0700

    test: click join game

--- test/sleeping_queens_interface_web/live/home_live_test.ex
+++ test/sleeping_queens_interface_web/live/home_live_test.ex
@@ -3,6 +3,9 @@ defmodule SleepingQueensInterfaceWeb.HomeLiveTest do
 
   import Phoenix.LiveViewTest
 
+  @player1 "player1"
+  @player2 "player2"
+
   test "renders the home page", %{conn: conn} do
     {:ok, view, _html} = live(conn, "/")
 
@@ -14,22 +17,6 @@ defmodule SleepingQueensInterfaceWeb.HomeLiveTest do
     {:ok, view, _html} = live(conn, "/")
 
     assert render(view) =~ "Create Game"
-  end
-
-  test "redirects to game page with random game code when creating a game", %{conn: conn} do
-    {:ok, view, _html} = live(conn, "/")
-
-    render_click(view, "create_game", %{"player_name" => "Neil"})
-    {path, _flash} = assert_redirect view
-
-    # checks for /game/<ANY_4_CHARACTERS>/ path
-    assert path =~ ~r/game\/([A-Za-z0-9]{4})/
-  end
-
-  test "renders a join game button", %{conn: conn} do
-    {:ok, view, _html} = live(conn, "/")
-
-    assert render(view) =~ "Join Game"
   end
 
   # TODO>>>>
@@ -48,6 +35,24 @@ defmodule SleepingQueensInterfaceWeb.HomeLiveTest do
   #          |> render_click() =~ "Create New Game"
   # end
 
+  test "creating a game redirects to game page with random game id and provided player name",
+       %{conn: conn} do
+    {:ok, view, _html} = live(conn, "/")
+    render_click(view, "create_game", %{"player_name" => @player1})
+    {path, _flash} = assert_redirect(view)
+
+    # checks for /game/<ANY_4_CHARACTERS>/
+    assert path =~ ~r/game\/([A-Za-z0-9]{4})/
+    assert path =~ @player1
+  end
+
+  test "renders a join game button", %{conn: conn} do
+    {:ok, view, _html} = live(conn, "/")
+
+    assert render(view) =~ "Join Game"
+  end
+
+  # TODO>>>>
   # test "shows join game modal when button is clicked", %{conn: conn} do
   #   {:ok, view, _html} = live(conn, "/")
   #
@@ -61,4 +66,33 @@ defmodule SleepingQueensInterfaceWeb.HomeLiveTest do
   #   # Assert that the join game modal is now present
   #   assert view |> find_modal("join_game_modal") |> is_not_nil()
   # end
+
+  test "joining a game redirects to game page with provided game id and player name",
+       %{conn: conn} do
+    # create game with player 1
+    {:ok, view1, _html} = live(conn, "/")
+    render_click(view1, "create_game", %{"player_name" => @player1})
+    {path, _flash} = assert_redirect(view1)
+    game_id = extract_game_id(path)
+
+    # join game with player 2
+    {:ok, view2, _html} = live(conn, "/")
+
+    render_click(view2, "join_game", %{
+      "game_id" => game_id,
+      "player_name" => @player2
+    })
+
+    {path, _flash} = assert_redirect(view2)
+
+    # checks for /game/<ANY_4_CHARACTERS>/ path
+    assert path =~ "/game/#{game_id}/#{@player2}"
+  end
+
+  # Returns game id from a path structured like "/game/ABCD/player1"
+  defp extract_game_id(path) do
+    path
+    |> String.split("/")
+    |> Enum.at(2)
+  end
 end
