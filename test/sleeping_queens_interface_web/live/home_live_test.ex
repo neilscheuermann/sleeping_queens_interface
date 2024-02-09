9da564c14615e6fbb618f9a190c5920b684460e2
Neil Scheuermann <neilscheuermann@gmail.com>
2024-02-08 20:24:04 -0700

    test trying to join started game

--- test/sleeping_queens_interface_web/live/home_live_test.ex
+++ test/sleeping_queens_interface_web/live/home_live_test.ex
@@ -2,10 +2,13 @@ defmodule SleepingQueensInterfaceWeb.HomeLiveTest do
   use SleepingQueensInterfaceWeb.ChannelCase
   use SleepingQueensInterfaceWeb.ConnCase
 
+  alias SleepingQueensEngine.Game
+
   import Phoenix.LiveViewTest
 
   @player1_name "player1"
   @player2_name "player2"
+  @player3_name "player3"
 
   test "renders the home page", %{conn: conn} do
     {:ok, view, _html} = live(conn, "/")
@@ -19,22 +22,6 @@ defmodule SleepingQueensInterfaceWeb.HomeLiveTest do
 
     assert render(view) =~ "Create Game"
   end
-
-  # TODO>>>>
-  # test "shows create game modal when button is clicked", %{conn: conn} do
-  #   {:ok, view, _html} = live(conn, "/")
-  #
-  #   # Assert that the create game modal is initially not present
-  #   # refute render(view) =~ "Create New Game"
-  #   # assert has_element?(view, ~s{[id="create_game_modal"][class="relative z-50 hidden"]})
-  #   # # assert view |> find_modal("create_game_modal") |> is_nil()
-  #
-  #   # Click the "Create Game" button
-  #   # Assert that the create game modal is now present
-  #   assert view
-  #          |> element("button", "Create Game")
-  #          |> render_click() =~ "Create New Game"
-  # end
 
   test "creating a game redirects to game page with random game id and provided player name",
        %{conn: conn} do
@@ -52,21 +39,6 @@ defmodule SleepingQueensInterfaceWeb.HomeLiveTest do
 
     assert render(view) =~ "Join Game"
   end
-
-  # TODO>>>>
-  # test "shows join game modal when button is clicked", %{conn: conn} do
-  #   {:ok, view, _html} = live(conn, "/")
-  #
-  #   # Assert that the join game modal is initially not present
-  #   assert view |> find_modal("join_game_modal") |> is_nil()
-  #
-  #   # Click the "Join Game" button
-  #   view
-  #   |> click_button("Join Game")
-  #
-  #   # Assert that the join game modal is now present
-  #   assert view |> find_modal("join_game_modal") |> is_not_nil()
-  # end
 
   test "joining a game redirects to game page with provided game id and player name",
        %{conn: conn} do
@@ -86,7 +58,6 @@ defmodule SleepingQueensInterfaceWeb.HomeLiveTest do
 
     {path, _flash} = assert_redirect(view2)
 
-    # checks for /game/<ANY_4_CHARACTERS>/ path
     assert path =~ "/game/#{game_id}/#{@player2_name}"
   end
 
@@ -113,6 +84,72 @@ defmodule SleepingQueensInterfaceWeb.HomeLiveTest do
     assert_receive({:table_updated, table})
     Enum.each(table.players, &(&1.name in [@player1_name, @player2_name]))
   end
+
+  test "cannot join a game that has already started",
+       %{conn: conn} do
+    # create game with player 1
+    {:ok, view1, _html} = live(conn, "/")
+    render_click(view1, "create_game", %{"player_name" => @player1_name})
+    {path, _flash} = assert_redirect(view1)
+    game_id = extract_game_id(path)
+
+    # join game with player 2
+    {:ok, view2, _html} = live(conn, "/")
+
+    render_click(view2, "join_game", %{
+      "game_id" => game_id,
+      "player_name" => @player2_name
+    })
+
+    {path, _flash} = assert_redirect(view2)
+
+    assert path =~ "/game/#{game_id}/#{@player2_name}"
+
+    # Start game
+    via = Game.via_tuple(game_id)
+    {:ok, %{table: table}} = Game.get_state(via) |> IO.inspect(label: ">>>>state")
+
+    # player 3 tries to join game but doesn't get redirected
+    {:ok, view3, _html} = live(conn, "/")
+
+    render_click(view3, "join_game", %{
+      "game_id" => game_id,
+      "player_name" => @player3_name
+    })
+
+    :ok = refute_redirected view3, "/game/#{game_id}/#{@player3_name}"
+  end
+
+  # TODO>>>>
+  # test "shows join game modal when button is clicked", %{conn: conn} do
+  #   {:ok, view, _html} = live(conn, "/")
+  #
+  #   # Assert that the join game modal is initially not present
+  #   assert view |> find_modal("join_game_modal") |> is_nil()
+  #
+  #   # Click the "Join Game" button
+  #   view
+  #   |> click_button("Join Game")
+  #
+  #   # Assert that the join game modal is now present
+  #   assert view |> find_modal("join_game_modal") |> is_not_nil()
+  # end
+  #
+  # TODO>>>>
+  # test "shows create game modal when button is clicked", %{conn: conn} do
+  #   {:ok, view, _html} = live(conn, "/")
+  #
+  #   # Assert that the create game modal is initially not present
+  #   # refute render(view) =~ "Create New Game"
+  #   # assert has_element?(view, ~s{[id="create_game_modal"][class="relative z-50 hidden"]})
+  #   # # assert view |> find_modal("create_game_modal") |> is_nil()
+  #
+  #   # Click the "Create Game" button
+  #   # Assert that the create game modal is now present
+  #   assert view
+  #          |> element("button", "Create Game")
+  #          |> render_click() =~ "Create New Game"
+  # end
 
   # Returns game id from a path structured like "/game/ABCD/player1_name"
   defp extract_game_id(path) do
